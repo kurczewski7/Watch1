f069fc36744cd4882636356cbfba29bb4a10d80b
Sławomir Kurczewski <kurczewski7@gmail.com>
2023-09-29 22:34:01 +0200

    Initial Commit

---
+++ Watch1 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watch1 Watch App
+//
+//  Created by Sławek K on 29/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watch1 Watch App/Watch1App.swift
@@ -0,0 +1,17 @@
+//
+//  Watch1App.swift
+//  Watch1 Watch App
+//
+//  Created by Sławek K on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Watch1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
